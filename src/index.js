b4a151d9da6c4109a704ed75e291aabe08d135e4
Ambor <saltbo@foxmail.com>
2020-06-24 15:41:52 +0800

    feat: first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// const fs = require('fs');
+const path = require('path');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const octokit = github.getOctokit("myToken")
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const dist = core.getInput('dist');
+    core.debug(`ISSUES_DIST = '${dist}'`)
+
+    // GitHub workspace
+    let githubWorkspacePath = process.env['GITHUB_WORKSPACE']
+    if (!githubWorkspacePath) {
+      throw new Error('GITHUB_WORKSPACE not defined')
+    }
+    githubWorkspacePath = path.resolve(githubWorkspacePath)
+    core.debug(`GITHUB_WORKSPACE = '${githubWorkspacePath}'`)
+
+    const { data: issues } = await octokit.issues.listForRepo({
+      owner: 'saltbo',
+      repo: 'blog',
+    });
+    console.log(issues)
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
